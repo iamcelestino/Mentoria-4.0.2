bdf86f69d2ed3f19a6fd3f6903a5c77550258687
iamcelestino <trcelestino488@gmail.com>
2024-05-15 14:52:40 -0700

    style: add waves to setions

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
       extend: {},
+      with: {
+        'full-plus-1.3px': 'calc(100% + 1.3px)'
+      }, 
       container: {
         center: true,
         padding: '2rem'
