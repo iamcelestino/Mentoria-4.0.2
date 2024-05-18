d016ceb004bcb72fb7eec1919579fdccbe2f1f81
iamcelestino <trcelestino488@gmail.com>
2024-05-18 10:32:05 -0700

    style: add custm-underline

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        fontFamily: {
+          'times': ['Times New Roman', 'serif'],
+        },
+      },
       with: {
         'full-plus-1.3px': 'calc(100% + 1.3px)'
       }, 
